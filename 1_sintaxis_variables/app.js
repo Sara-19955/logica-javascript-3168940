4550f29bdb91b79114b686364dcb015524f2a410
sara <saratorovalencia71@gmail.com>
2025-07-31 12:30:31 -0500

    repositorio js versión 1

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,27 @@
+//El alert muestra una ventana emergente en el navegador
+//alert("hola mundo");
+//prompt Muestra una caja para mostrar información en el navegador
+//prompt("dime tu nombre")
+
+//Variable declarada con let
+let personaje ="perrito"
+//Crear variable puntaje y asignar valor de 0
+//Los valores numericos van sin comillas 
+let puntaje = 0
+//Crear variable shots y asignar valor de 3
+let shots = 3
+    recarga = 2
+console.log( "total balas" ,shots+ recarga) 
+
+//El usuario capturo 4 patos 
+puntaje+=4
+//El usuario gasto 2 balas
+shots -=2 
+//El usuario recargo su arma 
+recarga +=1
+recarga++ //++ permite sumar de a uno
+recarga++ //++ permite sumar de a uno
+
+console.log("puntaje:",puntaje)//4
+console.log ("shots:",shots)//1
+console.log("recarga:",recarga)//3
